9865c97a734a12f8a87acc8706a24f11e8683876
Don Choppy <wxtem@hotmail.com>
2023-01-24 22:13:18 +0000

    changed makefile for bash file

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,5 +1,17 @@
+import os
+import re
 import cv2
+import json
 import numpy as np
+import pandas as pd
+from math import pi
+import seaborn as sns
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from augmentations import avg_aug
+from imutils.paths import list_images
+from sklearn.utils import class_weight
+from sklearn.model_selection import train_test_split
 
 def load_image(path, colorspace='RGB'):
     color = colorspace.lower()
@@ -23,69 +35,298 @@ def load_image(path, colorspace='RGB'):
         return None
     else: return cv2.cvtColor(image, spaces[color])
 
-def align(img, data):
-    # In case the image has more than 1 face, find the biggest face
-    biggest=0
-    if data != []:
-        for faces in data:
-            box = faces['box']            
-            # calculate the area in the image
-            area = box[3]  * box[2]
-            if area > biggest:
-                biggest = area
-                bbox = box                
-                keypoints = faces['keypoints']
-                left_eye = keypoints['left_eye']
-                right_eye = keypoints['right_eye']                 
+
+def plot_confusion_matrix2(cm, class_names, figsize=(16, 16), fontsize=36, draw_zeros=True, cmap="YlGnBu_r"):
+    sns.set_context("notebook", font_scale=4)
+    style = {
+        'axes.facecolor': "#EAEAF2",
+        'axes.edgecolor': "white",
+        'axes.labelcolor': ".15",
+        'figure.facecolor': "white",
+        'figure.edgecolor': "white",
+        'grid.color': "white",
+        'grid.linestyle': "-",
+        'legend.facecolor': ".8",
+        'legend.edgecolor': ".8",
+        'text.color': ".15",
+        'xtick.color': ".15",
+        'ytick.color': ".15",
+        'font.family': "sans-serif",
+        'font.sans-serif': "Helvetica",
+        'savefig.facecolor': "#EAEAF2",
+        'savefig.edgecolor': "#white",
+        'savefig.transparent': True,
+        'eer.color': ".66"
+    }
+    sns.set_style(style)
+
+    cm_sum = np.sum(cm, axis=1, keepdims=True)
+    cm_perc = cm / cm_sum.astype(float) * 100
+    annot = np.empty_like(cm).astype(str)
+    h, w = cm.shape[:2]
+    for i in range(w):
+        for j in range(h):
+            c = cm[i, j]
+            p = cm_perc[i, j]
+            if i == j:
+                s = cm_sum[i]
+                annot[i, j] = f"{c}/{s[0]}\n{p:.1f}%"
+            elif c == 0 and not draw_zeros:
+                annot[i, j] = ""
+            else:
+                annot[i, j] = f"{c}\n{p:.1f}%"
+    cm = pd.DataFrame(cm, index=class_names, columns=class_names)
+    cm.index.name = "Ground Truth"
+    cm.columns.name = "Predicted"
+    _, ax = plt.subplots(figsize=figsize)
+    heatmap = sns.heatmap(cm, cmap=cmap, annot=annot, fmt="",
+                          annot_kws={'size': fontsize}, ax=ax)
+    fig = heatmap.get_figure()
+    fig.patch.set_alpha(0)
+
+    return fig
+
+def _parse_function_with_DA(impath, label, shape):
+    # instantiate augmentation here to overcome "not-changing-random-seed" bug
+    # this could lead to an speed decrease or an overheap, I dont know
+    aug = avg_aug() # think about deleting aug after use?
+    # no problems detected during training nor validation
+
+    # image reading
+    image = tf.io.read_file(impath)
+    image = tf.io.decode_png(image, channels=3)
+    image = tf.cast(image, tf.float32)
+    image = tf.image.resize(image, shape)
+    image = tf.cast(image, tf.uint8)
+    image = tf.numpy_function(func=aug.augment_image, inp=[image], Tout=tf.uint8)
+    image = tf.cast(image, tf.float32)
+    image = image / 255.
+    return image, label
+
+def _parse_function_without_DA(impath, label, shape):
+    # image reading
+    image_string = tf.io.read_file(impath)
+    image_decoded = tf.io.decode_png(image_string, channels=3)
+    image = tf.cast(image_decoded, tf.float32)
+    image = tf.image.resize(image, shape)
+    image = image / 255.
+    return image, label
+
+# returns tf.data
+def load_dataset(
+    db_dict=None, db_key='flickr', multiclass=False, 
+    data_augmentation=False, shape=(224, 224), color='rgb', 
+    load_csv=False, return_class_weights=False, batch_size=16
+):
+    
+    buffer_size = 10101101
+    db_dir = db_dict.get(db_key)
+    _classes = ('attack', 'bonafide') if not multiclass else ('bonafide', 'print', 'screen')
+    _bf_index = _classes.index('bonafide')
+    imlist = [*list_images(db_dir)]
+    
+    if multiclass:
+        # generate multiclass_y vector
+        X, y = [], []
+        regex_str = "|".join(_classes)
         
-        lx,ly = left_eye        
-        rx,ry = right_eye
-        dx = rx-lx
-        dy = ry-ly
-        tan = dy/dx
-        theta = np.arctan(tan)
-        theta = np.degrees(theta)    
-        img = rotate_bound(img, theta)        
-        return img
-    
-    else:
-        return None
+        for imdir in imlist:
+            regex_res = re.search(regex_str, imdir)
+            regex_class = regex_res.group()
+            _id_class = _classes.index(regex_class)
+            X.append(imdir)
+            y.append(_id_class)
+        
+        X = np.array(X)
+        class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y), y=y)
+        class_weights = dict(enumerate(class_weights))
+        y = tf.keras.utils.to_categorical(y)
 
-def crop_image(img, data): 
-    
-    #y=box[1] h=box[3] x=box[0] w=box[2]   
-    biggest=0
-    
-    if data != []:
-        for faces in data:
-            box = faces['box']            
-            # calculate the area in the image
-            area = box[3] * box[2]
-            if area > biggest:
-                biggest = area
-                bbox = box 
-        bbox[0] = 0 if bbox[0] < 0 else bbox[0]
-        bbox[1] = 0 if bbox[1] < 0 else bbox[1]
-        img = img[ bbox[1]: bbox[1] + bbox[3], bbox[0]: bbox[0] + bbox[2] ]
-        return img
     else:
-        return None
+        bf_list, att_list = [], []
+        for imdir in imlist:
+            if _classes[_bf_index] in imdir.split(os.sep): # if 'bonafide' in imdir
+                bf_list.append(imdir)
+            else: # identify attack class
+                att_list.append(imdir)
+                
+        bf_y = np.ones(len(bf_list))
+        att_y = np.zeros(len(att_list)) # use labelencoder here
+        # split|load data into train|test|val
+        X = np.concatenate([att_list, bf_list])
+        y = np.concatenate([att_y, bf_y])
+        class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y), y=y)
+        class_weights = dict(enumerate(class_weights))
+        y = tf.keras.utils.to_categorical(y)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=42) # 0.25 x 0.8 = 0.2
+    
+    # train dataset
+    train_dataset = tf.cast(X_train, dtype=tf.string)
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_dataset, tf.cast(y_train, tf.float32)))
+    train_dataset = train_dataset.shuffle(buffer_size)
+    train_dataset = train_dataset.map(lambda x, y: _parse_function_with_DA(x, y, shape), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    train_dataset = train_dataset.cache()
+    train_dataset = train_dataset.batch(batch_size)
+    
+    # val dataset
+    val_dataset = tf.cast(X_val, dtype=tf.string)
+    val_dataset = tf.data.Dataset.from_tensor_slices((val_dataset, tf.cast(y_val, tf.float32)))
+    val_dataset = val_dataset.shuffle(buffer_size)
+    val_dataset = val_dataset.map(lambda x, y: _parse_function_without_DA(x, y, shape), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    val_dataset = val_dataset.cache()
+    val_dataset = val_dataset.batch(1)
+    
+    # test dataset
+    test_dataset = tf.cast(X_test, dtype=tf.string)
+    test_dataset = tf.data.Dataset.from_tensor_slices((test_dataset, tf.cast(y_test, tf.float32)))
+    test_dataset = test_dataset.shuffle(buffer_size)
+    test_dataset = test_dataset.map(lambda x, y: _parse_function_without_DA(x, y, shape), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    test_dataset = test_dataset.cache()
+    test_dataset = test_dataset.batch(1)
+    
+    if return_class_weights:
+        return train_dataset, val_dataset, test_dataset, class_weights
+    else:
+        return train_dataset, val_dataset, test_dataset, None
 
-def rotate_bound(image, angle):
-    # rotates an image by the degree angle
-    # grab the dimensions of the image and then determine the center
-    (h, w) = image.shape[:2]
-    (cX, cY) = (w // 2, h // 2)
-    # grab the rotation matrix (applying the angle to rotate clockwise), then grab the sine and cosine
-    # (i.e., the rotation components of the matrix)
-    M = cv2.getRotationMatrix2D((cX, cY), angle, 1.0)
-    cos = np.abs(M[0, 0])
-    sin = np.abs(M[0, 1]) 
-    # compute the new bounding dimensions of the image
-    nW = int((h * sin) + (w * cos))
-    nH = int((h * cos) + (w * sin)) 
-    # adjust the rotation matrix to take into account translation
-    M[0, 2] += (nW / 2) - cX
-    M[1, 2] += (nH / 2) - cY 
-    # perform the actual rotation and return the image
-    return cv2.warpAffine(image, M, (nW, nH)) 
+# define a plot function to plot history scores
+def plot_history(history, title, save_path, figsize=(12, 6), font_scale=1.5, linewidth=3.5, arcloss=False):
+    with sns.plotting_context(
+        "notebook", font_scale=font_scale, rc={"lines.linewidth": linewidth}
+    ):
+        if arcloss:
+            fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=figsize)
+            ax1.set_title('Loss')
+            ax1.set_ylabel("Loss")
+            ax1.set_xlabel("Epochs")
+            
+            ax2.set_title('Acc')
+            ax2.set_title(title)
+            ax2.set_ylabel("Acc")
+            ax2.set_xlabel("Epochs")
+            ax2.set_ylim([-0.1, 1.1])
+            
+            sns.lineplot(history.history["loss"], label="Train Loss", ax=ax1, linestyle='--')
+            sns.lineplot(history.history["val_loss"], label="Val. Loss", ax=ax1, linestyle='--')
+            sns.lineplot(history.history["acc"], label="Train Acc.", ax=ax2, linestyle='--')
+            sns.lineplot(history.history["val_acc"], label="Val. Acc.", ax=ax2, linestyle='--')
+        else:
+            fig, ax = plt.subplots(figsize=figsize)
+            ax.set_title(title)
+            ax.set_ylabel("Loss/Acc.")
+            ax.set_xlabel("Epochs")
+            plt.ylim([-0.1, 1.1])
+            sns.lineplot(history.history["loss"], label="Train Loss", ax=ax, linestyle='--')
+            sns.lineplot(history.history["val_loss"], label="Val. Loss", ax=ax, linestyle='--')
+            sns.lineplot(history.history["acc"], label="Train Acc.", ax=ax, linestyle='--')
+            sns.lineplot(history.history["val_acc"], label="Val. Acc.", ax=ax, linestyle='--')
+        fig.savefig(save_path)
+
+
+# Define ArchLoss
+# ref: https://www.kaggle.com/code/chankhavu/keras-layers-arcface-cosface-adacos/notebook7
+# ref: https://github.com/yinguobing/arcface/blob/main/train.py
+
+class L2Normalization(tf.keras.layers.Layer):
+    """This layer normalizes the inputs with l2 normalization."""
+
+    def __init__(self, **kwargs):
+        super(L2Normalization, self).__init__(**kwargs)
+
+    @tf.function
+    def call(self, inputs):
+        inputs = tf.nn.l2_normalize(inputs, axis=1)
+
+        return inputs
+
+    def get_config(self):
+        config = super().get_config()
+        return config
+
+class ArcLayer(tf.keras.layers.Layer):
+    """Custom layer for ArcFace.
+    This layer is equivalent a dense layer except the weights are normalized.
+    """
+
+    def __init__(self, units, kernel_regularizer=None, **kwargs):
+        super(ArcLayer, self).__init__(**kwargs)
+        self.units = units
+        self.kernel_regularizer = kernel_regularizer
+
+    def build(self, input_shape):
+        self.kernel = self.add_weight(shape=[input_shape[-1], self.units],
+                                      dtype=tf.float32,
+                                      initializer=tf.keras.initializers.HeNormal(),
+                                      regularizer=self.kernel_regularizer,
+                                      trainable=True,
+                                      name='kernel')
+        self.built = True
+
+    @tf.function
+    def call(self, inputs):
+        weights = tf.nn.l2_normalize(self.kernel, axis=0)
+        return tf.matmul(inputs, weights)
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"units": self.units,
+                       "kernel_regularizer": self.kernel_regularizer})
+        return config
+
+class ArcLoss(tf.keras.losses.Loss):
+    """Additive angular margin loss.
+    Original implementation: https://github.com/luckycallor/InsightFace-tensorflow
+    """
+
+    def __init__(self, margin=0.5, scale=64, name="arcloss"):
+        """Build an additive angular margin loss object for Keras model."""
+        super().__init__(name=name)
+        self.margin = margin
+        self.scale = scale
+        self.threshold = tf.math.cos(pi - margin)
+        self.cos_m = tf.math.cos(margin)
+        self.sin_m = tf.math.sin(margin)
+
+        # Safe margin: https://github.com/deepinsight/insightface/issues/108
+        self.safe_margin = self.sin_m * margin
+
+    @tf.function
+    def call(self, y_true, y_pred):
+
+        # Calculate the cosine value of theta + margin.
+        cos_t = y_pred
+        sin_t = tf.math.sqrt(1 - tf.math.square(cos_t))
+
+        cos_t_margin = tf.where(cos_t > self.threshold,
+                                cos_t * self.cos_m - sin_t * self.sin_m,
+                                cos_t - self.safe_margin)
+
+        # The labels here had already been onehot encoded.
+        mask = y_true
+        cos_t_onehot = cos_t * mask
+        cos_t_margin_onehot = cos_t_margin * mask
+
+        # Calculate the final scaled logits.
+        logits = (cos_t + cos_t_margin_onehot - cos_t_onehot) * self.scale
+
+        losses = tf.nn.softmax_cross_entropy_with_logits(y_true, logits)
+
+        return losses
+
+    def get_config(self):
+        config = super(ArcLoss, self).get_config()
+        config.update({"margin": self.margin, "scale": self.scale})
+        return config
+
+# serializer class
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer) or isinstance(obj, np.int64) or isinstance(obj, np.int32):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
